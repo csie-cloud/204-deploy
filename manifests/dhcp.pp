3eb4dee2f4ac0b661b7fc85d32214570f807cb25
CTinRay <b03902072@ntu.edu.tw>
2016-04-09 19:17:53 +0800

    dhcp pxe setting and domain name

--- manifests/dhcp.pp
+++ manifests/dhcp.pp
@@ -1,14 +1,20 @@
-class creator::dhcp{
-    class{ 'dhcp':
-	interfaces => ['enp11s0f0.40']
-    }
+class creator::dhcp( $domain_name ){
 
-    
+  class { 'dhcp':
+    dnsdomain    => [
+      $dmain_name
+    ],
+    nameservers  => ['192.168.217.254'],
+    interfaces   => ['enp11s0f0.40'],
+    pxeserver    => '192.168.217.254',
+    pxefilename  => 'bootstrap.ipxe',
+  }    
 
-    dhcp::pool{ 'ops.dc1.example.net':
-        network => '192.168.0.0',
+  dhcp::pool{ 'ops.dc1.example.net':
+    network => '192.168.0.0',
 	mask    => '255.255.0.0',
+    domain_name => $domain_name,
 	range   => '192.168.215.0 192.168.215.100',
-    }
+  }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
 class creator{
-   include ::password
-   include ::creator::razor   
-   include ::creator::dns
-   include ::creator::dhcp
+  $domain_name = "cloudy.csie.ntu.edu.tw"
+  include ::password
+  include ::creator::razor   
+  include ::creator::dns
+  class{ '::creator::dhcp':
+    domain_name => $domain_name
+  }
 }
